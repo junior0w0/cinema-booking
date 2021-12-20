6a16a1115012948d03673624d2beed387d8c0a7b
junior0w0 <92807269+junior0w0@users.noreply.github.com>
2021-12-20 23:17:35 +0700

    Update mainbody.cpp

--- mainbody.cpp
+++ mainbody.cpp
@@ -3,9 +3,63 @@
 #include <vector>
 #include <stdlib.h> 
 #include <windows.h>
-
+#include <array>
+#include <vector>
 
 using namespace std;
+
+class Film
+{
+  public:
+  void PengisianFilm()
+  { 
+      int nomorfilmterakhir;
+      int jumlahfilm;
+      cout << "Isikan jumlah film: ";
+      cin >> jumlahfilm;
+
+      string film[jumlahfilm];
+      int kuotafilm[jumlahfilm];
+      string waktufilmmulai[jumlahfilm];
+      string waktufilmselesai[jumlahfilm];
+
+      if (nomorfilmterakhir != 0)
+      {
+        for (int i = nomorfilmterakhir+1; i < (jumlahfilm + nomorfilmterakhir); i++)
+        {
+          cout << "Isikan nama film no-" << i+1 << "\n";
+          cin >> film[i];
+          cout << "Kuota: ";
+          cin >> kuotafilm[i];
+          cout << "Waktu mulai: ";
+          cin >> waktufilmmulai[i];
+          cout << "Waktu selesai: ";
+          cin >> waktufilmselesai[i];
+        }
+      } else
+        {
+          for (int i = 0; i < jumlahfilm; i++)
+          {
+              cout << "Isikan nama film no-" << i+1 << "\n";
+              cin >> film[i];
+              cout << "Kuota: ";
+              cin >> kuotafilm[i];
+              cout << "Waktu mulai: ";
+              cin >> waktufilmmulai[i];
+              cout << "Waktu selesai: ";
+              cin >> waktufilmselesai[i];
+          }        
+        }
+
+      cout << "Film tersedia:\n";
+      for (int i = 0; i < jumlahfilm; i++)
+      {
+          cout << i+1 << ". " << film[i] << "\nJumlah kursi tersedia: " << kuotafilm[i] << "\nWaktu tayang: " << waktufilmmulai[i] << " sampai " << waktufilmselesai[i] << "\n";
+      }
+      int nomorfilmterakhir = sizeof(film)/sizeof(film[0]);
+
+  }
+};
 
 class Pembeli
 {     
@@ -13,68 +67,54 @@ class Pembeli
     char kolom;
     int baris;
     string nama;
-};
-
-class Film
-{
-  public:
-    void PengisianFilm()
+  void PengisianDataPembeli()
+  {
+    //deklarasi vektor
+    vector<Pembeli> IdentitasPembeli;
+        bool lanjut = true;
+    while (lanjut)
     {
-     int jumlahfilm;
-     cin >> jumlahfilm;
+      //Memanggil class
+      Pembeli Pembeli1;
+      cout << "Masukkan kolom: \n";
+      cin >> Pembeli1.kolom;
+      cout << "Masukkan baris: \n";
+      cin >> Pembeli1.baris;
+      cout << "Masukkan nama: \n";
+      cin >> Pembeli1.nama;
 
-     string film[jumlahfilm];
-     
-     for (int i = 0; i < jumlahfilm; i++)
-     {
-      cin >> film[i];
-     }
-     for (int i = 0; i < jumlahfilm; i++)
-     {
-      cout << film[i];
-     }
+      
+
+    //Menambah variabel
+      cout << "\nKursi: " << Pembeli1.kolom << Pembeli1.baris << "\nNama: " << Pembeli1.nama << endl;
+
+    
+    
+    cout << "Tekan ENTER untuk melanjutkan";
+    getch();
+    system("CLS");
+      
+      IdentitasPembeli.push_back(Pembeli1);
+      
+      //Apakah mau lanjut atau tidak
+      char opsi;
+      do
+      {
+      cout << "Apakah masih akan menginput data(y/n)?\n";
+      cin >> opsi;
+      } while (!(opsi == 'y' || opsi == 'n'));
+    
+      if (opsi == 'n')
+      {
+      lanjut = false;
+      }
     }
+  }
 };
 
 
 int main()
 {
- //deklarasi vektor
- vector<Pembeli> IdentitasPembeli;
-    bool lanjut = true;
- while (lanjut)
- {
-  //Memanggil class
-  Pembeli Pembeli1;
-  cout << "Masukkan kolom: \n";
-  cin >> Pembeli1.kolom;
-  cout << "Masukkan baris: \n";
-  cin >> Pembeli1.baris;
-  cout << "Masukkan nama: \n";
-  cin >> Pembeli1.nama;
-
-  
-
- //Menambah variabel
-  cout << "\nKursi: " << Pembeli1.kolom << Pembeli1.baris << "\nNama: " << Pembeli1.nama << endl;
  
- cout << "Tekan ENTER untuk melanjutkan";
- getch();
- system("CLS");
-  
-  IdentitasPembeli.push_back(Pembeli1);
-  
-  //Apakah mau lanjut atau tidak
-  char opsi;
-  do
-  {
-   cout << "Apakah masih mau lanjut(y/n)?\n";
-   cin >> opsi;
-  } while (!(opsi == 'y' || opsi == 'n'));
- 
-  if (opsi == 'n')
-  {
-   lanjut = false;
-  }
- }
 }
+
